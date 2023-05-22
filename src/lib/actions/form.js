b15d20f01bab836ab84f07444f8286f6dd1440c0
Karim Jordan <karimfromjordan@gmail.com>
2023-05-22 18:02:59 +0300

    rename `event` to `e`

--- src/lib/actions/form.js
+++ src/lib/actions/form.js
@@ -3,27 +3,25 @@ export default function form(form_element) {
 	form_element.setAttribute('novalidate', true);
 
 	/** @param {SubmitEvent} event */
-	async function handle_submit(event) {
-		event.preventDefault();
+	async function handle_submit(e) {
+		e.preventDefault();
 
 		const is_valid = true;
 
 		if (!is_valid) return;
 
-		event.target.dispatchEvent(new CustomEvent('form-submission-start', { bubbles: true }));
+		e.target.dispatchEvent(new CustomEvent('form-submission-start', { bubbles: true }));
 
-		const response = await fetch(event.target.getAttribute('action'), {
-			method: event.target.getAttribute('method'),
-			body: new FormData(event.target)
+		const response = await fetch(e.target.getAttribute('action'), {
+			method: e.target.getAttribute('method'),
+			body: new FormData(e.target)
 		});
 
-		event.target.dispatchEvent(new CustomEvent('form-submission-end', { bubbles: true }));
+		e.target.dispatchEvent(new CustomEvent('form-submission-end', { bubbles: true }));
 
 		const json = await response.json();
 
-		event.target.dispatchEvent(
-			new CustomEvent('form-response', { bubbles: true, detail: { json } })
-		);
+		e.target.dispatchEvent(new CustomEvent('form-response', { bubbles: true, detail: { json } }));
 	}
 
 	form_element.addEventListener('submit', handle_submit);
