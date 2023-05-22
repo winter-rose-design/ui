e7b6f60861d972ed11756af03536f389830f8f08
Karim Jordan <karimfromjordan@gmail.com>
2023-05-22 17:34:45 +0300

    first draft

---
+++ src/lib/actions/form.js
@@ -0,0 +1,27 @@
+/** @param {HTMLElement} form_element */
+export default function form(form_element) {
+	form_element.setAttribute('novalidate', true);
+
+	/** @param {SubmitEvent} event */
+	async function handle_submit(event) {
+		event.preventDefault();
+
+		const is_valid = true;
+
+		if (!is_valid) return;
+
+		event.target.dispatchEvent(new CustomEvent('form:submission:start', { bubbles: true }));
+
+		const response = await fetch(event.target.getAttribute('action'), new FormData(event.target));
+
+		event.target.dispatchEvent(new CustomEvent('form:submission:end', { bubbles: true }));
+
+		const json = response.ok ? await response.json() : null;
+
+		event.target.dispatchEvent(
+			new CustomEvent('form:response', { bubbles: true, detail: { json } })
+		);
+	}
+
+	form_element.addEventListener('submit', handle_submit);
+}
