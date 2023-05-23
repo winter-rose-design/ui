e5e0fb7ae59b2114fb40a9e44f17f233db2de85c
Karim Jordan <karimfromjordan@gmail.com>
2023-05-23 03:42:08 +0300

    create docs branch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+	<main>
+		<h1>UI</h1>
+	</main>
+</div>
+
+<style>
+	main {
+		max-width: 50rem;
+		margin-inline: auto;
+	}
+</style>
