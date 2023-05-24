98bd924a95ba7177bccc22c31013e3895d816081
Karim Jordan <karimfromjordan@gmail.com>
2023-05-24 07:22:46 +0300

    add arrow icons

--- src/lib/components/Context.svelte
+++ src/lib/components/Context.svelte
@@ -12,9 +12,21 @@
 		<svg style="display:none;">
 			<!-- Icons from https://lucide.dev/ -->
 			<defs>
-				<symbol id="ui-icon-arrow" viewBox="0 0 24 24">
+				<symbol id="ui-icon-arrow-up" viewBox="0 0 24 24">
 					<line x1="12" x2="12" y1="19" y2="5" />
 					<polyline points="5 12 12 5 19 12" />
+				</symbol>
+				<symbol id="ui-icon-arrow-down" viewBox="0 0 24 24">
+					<line x1="12" x2="12" y1="5" y2="19" />
+					<polyline points="19 12 12 19 5 12" />
+				</symbol>
+				<symbol id="ui-icon-arrow-left" viewBox="0 0 24 24">
+					<line x1="19" x2="5" y1="12" y2="12" />
+					<polyline points="12 19 5 12 12 5" />
+				</symbol>
+				<symbol id="ui-icon-arrow-right" viewBox="0 0 24 24">
+					<line x1="5" x2="19" y1="12" y2="12" />
+					<polyline points="12 5 19 12 12 19" />
 				</symbol>
 			</defs>
 		</svg>
